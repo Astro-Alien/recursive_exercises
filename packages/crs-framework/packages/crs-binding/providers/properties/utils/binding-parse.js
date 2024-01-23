5b9f61f9a5856bac3462d759c0b4dec066e25ff7
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2024-01-23 15:49:32 +0200

    Updated binding engine

--- packages/crs-framework/packages/crs-binding/providers/properties/utils/binding-parse.js
+++ packages/crs-framework/packages/crs-binding/providers/properties/utils/binding-parse.js
@@ -1,1 +1,30 @@
-async function d(n,s,a){const u=n.name.split("."),e=n.ownerElement,r=u[0];let i=n.value;crs.binding.utils.markElement(e,s),e.removeAttribute(n.name),e.setAttribute("data-field",i);const l=e.__uuid;let t=crs.binding.eventStore.getIntent("change",l);t==null&&(t={provider:a,value:{},dataDef:null}),t.value[i]=r;const c="change";crs.binding.eventStore.register(c,l,t),crs.binding.data.setCallback(e.__uuid,s.bid,[i],a),e.__events||=[],e.__events.push("change")}export{d as bindingParse};
+async function bindingParse(attr, context, provider) {
+  const parts = attr.name.split(".");
+  const element = attr.ownerElement;
+  const property = parts[0];
+  let path = attr.value;
+  crs.binding.utils.markElement(element, context);
+  element.removeAttribute(attr.name);
+  if (property == "value") {
+    element.setAttribute("data-field", path);
+  }
+  const uuid = element["__uuid"];
+  const event = "change";
+  const intentCollection = crs.binding.eventStore.getIntent(event, uuid);
+  const intent = {
+    provider,
+    value: { [path]: property },
+    dataDef: null
+  };
+  if (intentCollection != null) {
+    intentCollection.push(intent);
+  } else {
+    crs.binding.eventStore.register(event, uuid, intent);
+  }
+  crs.binding.data.setCallback(element["__uuid"], context.bid, [path], provider);
+  element.__events ||= [];
+  element.__events.push(event);
+}
+export {
+  bindingParse
+};
